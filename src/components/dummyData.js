1f4124c0aaf82acfdc829f3dbb3e42e094cabc4e
Mohamed Abdelmegeed <mohamedabdelmageed1095@gmail.com>
2023-05-14 11:04:27 +0300

    re-usable chart component

---
+++ src/components/dummyData.js
@@ -0,0 +1,50 @@
+export const usersData = [
+  {
+    name: 'Jan',
+    ActiveUsers: 4000,
+  },
+  {
+    name: 'Feb',
+    ActiveUsers: 3000,
+  },
+  {
+    name: 'Mar',
+    ActiveUsers: 2000,
+  },
+  {
+    name: 'April',
+    ActiveUsers: 2780,
+  },
+  {
+    name: 'May',
+    ActiveUsers: 1890,
+  },
+  {
+    name: 'Jun',
+    ActiveUsers: 2390,
+  },
+  {
+    name: 'July',
+    ActiveUsers: 3490,
+  },
+  {
+    name: 'Aug',
+    ActiveUsers: 5490,
+  },
+  {
+    name: 'Sept',
+    ActiveUsers: 2490,
+  },
+  {
+    name: 'Oct',
+    ActiveUsers: 1490,
+  },
+  {
+    name: 'Nov',
+    ActiveUsers: 3490,
+  },
+  {
+    name: 'Dec',
+    ActiveUsers: 6490,
+  },
+];
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Home.css';
 import FeaturedInfo from '../../components/FeaturedInfo/FeaturedInfo';
+import Chart from '../../components/Chart/Chart';
+import { usersData } from '../../components/dummyData';
 
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        title="Users Analytics"
+        data={usersData}
+        grid
+        dataKey="ActiveUsers"
+      />
     </div>
   );
 }
